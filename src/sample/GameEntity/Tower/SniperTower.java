85c2fe23fd53082272bb52e9e447433f3e920a6f
conngminhuet <56582551+conngminhuet@users.noreply.github.com>
2019-11-19 17:49:32 +0700

    Update SniperTower.java

--- src/sample/GameEntity/Tower/SniperTower.java
+++ src/sample/GameEntity/Tower/SniperTower.java
@@ -7,10 +7,10 @@ public class SniperTower extends Tower {
 
     public SniperTower(Pane layer, Image image, double x, double y, double r, double dx, double dy, double dr, double health, double speed) {
         super(layer, image, x, y, r, dx, dy, dr, health, speed);
-        this.targetRange = 500;
+        this.targetRange = 400;
         this.cost = 20;
         this.setDamage(50);
-        this.speed = 2;
+        this.speed = 2.5;
     }
 
 }
